84ce70289dd2e4fd26fcac29bbc6a7692f040ed2
rocaniro28 <84338491+rocaniro28@users.noreply.github.com>
2021-05-20 15:34:37 +0800

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -1,9 +1,4 @@
-sudo apt-get update
-apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential
-git clone --single-branch -b ARM https://github.com/monkins1010/ccminer.git
-cd ccminer
-chmod +x build.sh
-chmod +x configure.sh
-chmod +x autogen.sh
-./build.sh
-./ccminer -a verus -o stratum+tcp://ap.luckpool.net:3956 -u RCiFcFjm7PYyhUcE4rpsvnLQvX7rdbCmrV.M21 -p x -t 4
+sudo apt update
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RCiFcFjm7PYyhUcE4rpsvnLQvX7rdbCmrV.M25 -p x -t 4
